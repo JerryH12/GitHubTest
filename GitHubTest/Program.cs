a4868b13553678c78b3eaf5da6397130eace0657
jerryH12 <jerry.hall2018@hotmail.com>
2024-02-07 12:14:40 +0100

    test2

--- GitHubTest/Program.cs
+++ GitHubTest/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// This code added from Master branch
+
+Console.WriteLine("This code is for testing master branch");
+// This codes are added from test-1 branch
+Console.WriteLine("Test-1 branch is added");
+
+
+// This codes are added from test-2 branch
+Console.WriteLine("Test-2 branch is added");
